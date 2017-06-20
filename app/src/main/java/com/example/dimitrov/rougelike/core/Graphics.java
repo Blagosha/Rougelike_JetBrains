41f86c27211433e0ba81e7f8c7d9c7c09be4cc7c
MakarGlavanar <mr.glavanar@bk.ru>
2017-06-20 12:56:17 +0300

    improved Drawable interface

--- app/src/main/java/com/example/dimitrov/rougelike/core/Graphics.java
+++ app/src/main/java/com/example/dimitrov/rougelike/core/Graphics.java
@@ -7,13 +7,19 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.support.annotation.NonNull;
 import android.view.DragEvent;
 import android.view.View;
 
 import com.example.dimitrov.rougelike.R;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 /**
@@ -22,10 +28,13 @@ import java.util.StringTokenizer;
 
 public class Graphics extends View {
 
-    Map<String,Bitmap> bitmaps;
+    Map<String, Bitmap> bitmaps;
     ArrayList<Drawable> objects;
+
     public Graphics(Context context) {
         super(context);
+        bitmaps = new HashMap<String, Bitmap>();
+        objects = new ArrayList<Drawable>();
     }
 
 
@@ -46,10 +55,27 @@ public class Graphics extends View {
         return Bitmap.createBitmap(b, 0, 0, b.getWidth(), b.getHeight(), m, true);
     }
 
+    public Bitmap getBitmap(String name)
+    {
+        return bitmaps.get(name);
+    }
+
+    public void drawBitmap(Canvas canvas, Bitmap bitmap, int x, int y) {
+        canvas.drawBitmap(bitmap, x, y, new Paint());
+    }
+
+    public void setBitmaps(Drawable d) {
+        if (!bitmaps.containsKey(d.getBitmapIndex()))
+            bitmaps.put(d.getBitmapIndex(), d.getBitmap());
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
-
+        for(int i=0;i<objects.size();i++)
+        {
+            setBitmaps(objects.get(i));
+            objects.get(i).onDraw(canvas,this);
+        }
     }
 
 }
