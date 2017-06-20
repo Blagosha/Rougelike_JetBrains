a2943902a5906064f01dd97baa36ecfbb06f0776
Artyom Nurgaliev <artyomnurgaliev@mail.ru>
2017-06-20 11:17:48 +0300

    Character

---
+++ app/src/main/java/com/example/dimitrov/rougelike/Hero.java
@@ -0,0 +1,12 @@
+package com.example.dimitrov.rougelike;
+
+/**
+ * Created by Санчес on 20.06.2017.
+ */
+
+public class Hero extends Character{
+    public Hero(int x, int y, int hp) {
+        super(x, y, hp);
+    }
+
+}
---
+++ app/src/main/java/com/example/dimitrov/rougelike/Monster.java
@@ -0,0 +1,11 @@
+package com.example.dimitrov.rougelike;
+
+/**
+ * Created by Санчес on 20.06.2017.
+ */
+
+public class Monster extends Character {
+    public Monster(int x, int y, int hp) {
+        super(x, y, hp);
+    }
+}
