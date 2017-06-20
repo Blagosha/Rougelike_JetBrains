cea2f68bf2dcd9e5af283985a7f92797751b015f
Artyom Nurgaliev <artyomnurgaliev@mail.ru>
2017-06-20 10:58:18 +0300

    Bitmapwall and bitmapfloor

--- app/src/main/java/com/example/dimitrov/rougelike/Graphics.java
+++ app/src/main/java/com/example/dimitrov/rougelike/Graphics.java
@@ -1,8 +1,11 @@
 package com.example.dimitrov.rougelike;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.DragEvent;
 import android.view.View;
 
@@ -11,13 +14,24 @@ import android.view.View;
  */
 
 public class Graphics extends View {
+    Paint paint;
+
+    Bitmap bitmapfloor;
+    Bitmap bitmapwall;
 
     public Graphics(Context context) {
         super(context);
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        bitmapfloor = BitmapFactory.decodeResource(getResources(), R.mipmap.floor);
+        bitmapwall = BitmapFactory.decodeResource(getResources(), R.mipmap.wall);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+        canvas.drawBitmap(bitmapfloor, 50, 50, paint);
+        canvas.drawBitmap(bitmapwall,100,100,paint);
     }
+
 }
