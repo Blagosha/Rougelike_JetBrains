8819732e403ab0756afd2743f153e1e6d3f3d06b
Blagosha <dimitrovblagoi@gmail.com>
2017-06-21 14:22:36 +0300

    added constants

--- app/src/main/java/com/example/dimitrov/rougelike/objects/Stage.java
+++ app/src/main/java/com/example/dimitrov/rougelike/objects/Stage.java
@@ -22,11 +22,11 @@ import static com.example.dimitrov.rougelike.core.Toucher.sideSize;
 
 
 public class Stage implements GraphicsUser {
-    private final int forestIndificator = 0;
-    private final int wallIndificator = 1;
-    private final int floorIndificator = 2;
-    private final int chestIndificator = 3;
-    private final int mosterIndificator = 4;
+    private final int FOREST = 0;
+    private final int WALL = 1;
+    private final int FLOOR = 2;
+    private final int CHEST = 3;
+    private final int MOSTER = 4;
 
     public int cntRooms;
     public static int cellSideSize;
